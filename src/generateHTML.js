87bed2539c9d106bc60f9c7dbfc15a0677f6fe5a
Stephen Novelli <snovelli1021@gmail.com>
2022-09-11 19:16:53 -0700

    Created generateEmployees.js to return the individual employee selection and created inital outline for generateHTML.js.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,13 +17,12 @@ function createHTML(managers, engineers, interns) {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
             <link rel="stylesheet" type="text/css" href="./assets/style.css" />
-            <title>Employee Cards</title>
+            <title>Employee Profiles</title>
         </head>
         <body>
         <header>
-            <h1>Employee Cards</h1>
+            <h1>Employee Profiles</h1>
         </header>
         <div class="container">
             <div class="row">
@@ -44,7 +43,6 @@ function createHTML(managers, engineers, interns) {
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         </body>
         </html>`;
 }
