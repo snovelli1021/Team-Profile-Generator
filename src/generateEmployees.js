3ebe579a2ff202eeeeeb5ea32907108ab2892b34
Stephen Novelli <snovelli1021@gmail.com>
2022-09-13 21:57:53 -0700

    fixed return in subclasses.

--- src/generateEmployees.js
+++ src/generateEmployees.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const generateHTML = require("./generateHTML");
+const { generateHTML } = require("./generateHTML");
 const Employee = require("../lib/Employee.js");
 const Engineer = require("../lib/Engineer.js");
 const Intern = require("../lib/Intern.js");
@@ -20,7 +20,19 @@ function userInput() {
   ]);
 }
 
-function employeePrompt() {}
+function employeePrompt() {
+  return inquirer.prompt([
+    {
+      name: "role",
+      message: "What type of employee are you submitting?",
+      type: "list",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    { name: "name", message: "What is their name?", type: "input" },
+    { name: "email", message: "What is their email?", type: "input" },
+    { name: "id", message: "What is their employee ID?", type: "input" },
+  ]);
+}
 
 function managerPrompt() {
   return inquirer.prompt([
@@ -53,77 +65,59 @@ function internPrompt() {
 }
 
 function createEmployee() {
-  //   let employeePrompt = employeePrompt();
-  //   let employee = new Employee(
-  //     employeePrompt.name,
-  //     employeePrompt.email,
-  //     employeePrompt.id,
-  //     "Employee"
-  //   );
-  inquirer
-    .prompt([
-      {
-        name: "role",
-        message: "What type of employee do you want to add?",
-        type: "list",
-        choices: ["Manager", "Engineer", "Intern"],
-      },
-      { name: "name", message: "What is the employee's name?", type: "input" },
-      {
-        name: "email",
-        message: "What is the employee's email?",
-        type: "input",
-      },
-      { name: "id", message: "What is the employee's ID?", type: "input" },
-    ])
-    .then((answers) => {
-      if (employeePrompt.role === "Manager") {
-        let managerAnswer = managerPrompt();
-        let manager = new Manager(
-          employeePrompt.name,
-          employeePrompt.email,
-          employeePrompt.id,
-          employeePrompt.role,
-          managerAnswer.officeNumber
-        );
-        managersArr.push(manager);
-      } else if (employeePrompt.role === "Engineer") {
-        let engineerAnswer = engineerPrompt();
-        let engineer = new Engineer(
-          employeePrompt.name,
-          employeePrompt.email,
-          employeePrompt.id,
-          employeePrompt.role,
-          engineerAnswer.gitHubProfile
-        );
-        engineersArr.push(engineer);
-      } else if (employeePrompt.role === "Intern") {
-        let internAnswer = internPrompt();
-        let intern = new Intern(
-          employeePrompt.name,
-          employeePrompt.email,
-          employeePrompt.id,
-          employeePrompt.role,
-          internAnswer.school
-        );
-        internsArr.push(intern);
-      }
-      let employeeEntry = userInput();
-      if (employeeEntry.lastEntry === "No") {
-        return createEmployee();
-      } else if (
-        employeeEntry.lastEntry === "Yes" &&
-        managersArr.length > 0 &&
-        engineersArr.length > 0
-      ) {
-        generateHTML(managersArr, engineersArr, internsArr);
-      } else {
-        console.log(
-          "Please ensure you have at lease one Manager AND one Engineer before selecting 'Yes' again."
-        );
-      }
-    });
-  return createEmployee();
+  let employeePrompt = employeePrompt();
+  let employee = new Employee(
+    employeePrompt.name,
+    employeePrompt.email,
+    employeePrompt.id,
+    "Employee"
+  );
+
+  if (employeePrompt.role === "Manager") {
+    let managerAnswer = managerPrompt();
+    let manager = new Manager(
+      employeePrompt.name,
+      employeePrompt.email,
+      employeePrompt.id,
+      employeePrompt.role,
+      managerAnswer.officeNumber
+    );
+    managersArr.push(manager);
+  } else if (employeePrompt.role === "Engineer") {
+    let engineerAnswer = engineerPrompt();
+    let engineer = new Engineer(
+      employeePrompt.name,
+      employeePrompt.email,
+      employeePrompt.id,
+      employeePrompt.role,
+      engineerAnswer.gitHubProfile
+    );
+    engineersArr.push(engineer);
+  } else if (employeePrompt.role === "Intern") {
+    let internAnswer = internPrompt();
+    let intern = new Intern(
+      employeePrompt.name,
+      employeePrompt.email,
+      employeePrompt.id,
+      employeePrompt.role,
+      internAnswer.school
+    );
+    internsArr.push(intern);
+  }
+  let employeeEntry = userInput();
+  if (employeeEntry.lastEntry === "No") {
+    return createEmployee();
+  } else if (
+    employeeEntry.lastEntry === "Yes" &&
+    managersArr.length > 0 &&
+    engineersArr.length > 0
+  ) {
+    generateHTML(managersArr, engineersArr, internsArr);
+  } else {
+    console.log(
+      "Please make sure you have at lease one Manager AND one Engineer before selecting 'Yes' again."
+    );
+  }
 }
 
 module.exports = {
